e3a91f9a249e6f4d0773cb5d2aee43012770e24d
yongxiaolong <szphhyxl@126.com>
2015-11-23 22:32:40 +0800

    init game scene

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var path = require('path');
 var io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/html/index.html');

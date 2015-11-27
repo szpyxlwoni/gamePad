baadbebdbae20bbe7df00a1d593ce6688476b020
yongxiaolong <szphhyxl@126.com>
2015-11-28 00:20:49 +0800

    finish game logic

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var app = express();
 var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
+var _ = require('underscore');
+
+var players = [];
+var playerNumber = 0;
+var playerPosition = [0, 0];
+var returnValue = {sceneType:'ready', players:[]};
+var cards = [];
+var signs = [];
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -10,9 +18,274 @@ app.get('/', function(req, res){
   res.sendfile(__dirname + '/html/index.html');
 });
 
+var Card = function(name, type) {
+    this.name = name;
+    this.type = type;
+}
+
+var Sign = function(name, type, values) {
+	this.name = name;
+	this.type = type;
+	this.values = values;
+}
+
+var Player = function(id, state, socketId) {
+	this.id = id;
+	this.state = state;
+	this.socketId = socketId;
+	this.signs = [];
+	this.winSign = 0;
+	this.mostCamel = 0;
+	this.signs.push(new Sign("钻石", 1, []));
+	this.signs.push(new Sign("黄金", 2, []));
+	this.signs.push(new Sign("白银", 3, []));
+	this.signs.push(new Sign("皮革", 4, []));
+	this.signs.push(new Sign("香料", 5, []));
+	this.signs.push(new Sign("布匹", 6, []));
+	this.signs.push(new Sign("3张牌的奖励", 8, []));
+	this.signs.push(new Sign("4张牌的奖励", 9, []));
+	this.signs.push(new Sign("5张牌的奖励", 10, []));
+}
+
+function zeroSign(player) {
+	player.signs = [];
+	player.signs.push(new Sign("钻石", 1, []));
+	player.signs.push(new Sign("黄金", 2, []));
+	player.signs.push(new Sign("白银", 3, []));
+	player.signs.push(new Sign("皮革", 4, []));
+	player.signs.push(new Sign("香料", 5, []));
+	player.signs.push(new Sign("布匹", 6, []));
+	player.signs.push(new Sign("3张牌的奖励", 8, []));
+	player.signs.push(new Sign("4张牌的奖励", 9, []));
+	player.signs.push(new Sign("5张牌的奖励", 10, []));
+}
+
+function initCards() {
+	cards = [];
+	for (var i = 0; i < 6; i++) {
+		cards.push(new Card("黄金", 2));
+		cards.push(new Card("白银", 3));
+		cards.push(new Card("钻石", 1));
+	}
+	for (var i = 0; i < 8; i++) {
+		cards.push(new Card("香料", 5));
+		cards.push(new Card("布匹", 6));
+	}
+	for (var i = 0; i < 10; i++) {
+		cards.push(new Card("皮革", 4));
+	}
+	for (var i = 0; i < 8; i++) {
+		cards.push(new Card("骆驼", 7));
+	}
+	cards = _.shuffle(cards);
+}
+
+function initSigns() {
+	signs = [];
+	signs.push(new Sign("钻石", 1, [7,7,5,5,5]));
+	signs.push(new Sign("黄金", 2, [6,6,5,5,5]));	
+	signs.push(new Sign("白银", 3, [5,5,5,5,5]));
+	signs.push(new Sign("皮革", 4, [4]));//,3,2,1,1,1,1,1,1]));	
+	signs.push(new Sign("香料", 5, [5]));//,3,3,2,2,1,1]));	
+	signs.push(new Sign("布匹", 6, [5]));//,3,3,2,2,1,1]));
+	signs.push(new Sign("3张牌的奖励", 8, _.shuffle([3,3,2,2,2,1,1])));
+	signs.push(new Sign("4张牌的奖励", 9, _.shuffle([4,4,5,5,6,6])));
+	signs.push(new Sign("5张牌的奖励", 10, _.shuffle([8,8,9,10,10])));
+	signs.push(new Sign("最多骆驼的奖励", 7, 5));
+}
+
+var broadcast = function(msg){
+	_.each(players, function(v){
+	    var socket = _.findWhere(io.sockets.sockets, {id:v.socketId});
+	        if (socket) {
+		    msg.playerId = v.id;
+		    socket.emit('return.state', msg);
+		}
+	});
+};
+
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  var position = _.findIndex(playerPosition, function (v){
+	  return v == 0;
+  });
+  var id = position + 1;
+
+  if (position == -1) {
+      return;
+  }
+  socket.on('login', function(msg){
+      console.log('player ' + id + ' connected');
+      playerNumber++;
+      playerPosition[position] = 1;
+      players.push(new Player(id, 'logined', socket.id));
+      returnValue.sceneType = 'ready';
+      returnValue.players = players;
+      broadcast(returnValue);
+  });
+
+  socket.on('ready', function (){
+      console.log('player ' + id + ' ready');
+      _.map(players, function(v){
+	      if (v.id == id) {
+	          v.state = 'ready';
+	      }
+      });
+      if (players.length === 2 && !_.findWhere(players, {state:'logined'})) {
+          returnValue.sceneType = 'gaming';
+		  initCards();
+		  initSigns();
+		  returnValue.cards = cards;
+		  returnValue.players[0].currentCard = [];
+		  returnValue.players[1].currentCard = [];
+		  for (var i = 0; i < 5; i++) {
+			  returnValue.players[0].currentCard.push({type:cards.pop().type, isSelect:false});
+			  returnValue.players[1].currentCard.push({type:cards.pop().type, isSelect:false});
+		  }
+		  returnValue.market = [{type:7, isSelect:false},{type:7, isSelect:false},{type:7, isSelect:false},{type:cards.pop().type, isSelect:false},{type:cards.pop().type, isSelect:false}];
+		  returnValue.signs = signs;
+		  returnValue.turn = _.sample(players).id;
+      }
+      returnValue.players = players;
+      broadcast(returnValue);
+  });
+
+  socket.on('sell', function(msg){
+	  console.log('player ' + id + ' sell');
+	  console.log(msg);
+	  returnValue.sceneType = 'turnOver';
+	  returnValue.market = msg.market;
+	  returnValue.players = msg.players;
+	  returnValue.signs = msg.signs;
+	  _.map(returnValue.market, function(v){v.isSelect = false;return v});
+	  _.map(returnValue.players[0].currentCard, function(v){v.isSelect = false;return v});
+	  _.map(returnValue.players[1].currentCard, function(v){v.isSelect = false;return v});
+      broadcast(returnValue);
+  });
+
+  socket.on('getOne', function(msg){
+	  console.log('player ' + id + ' get one');
+	  console.log(msg);
+	  returnValue.sceneType = 'turnOver';
+      msg.market.push({type:cards.pop().type, isSelect:false})
+	  returnValue.market = msg.market;
+	  returnValue.players = msg.players;
+	  _.map(returnValue.market, function(v){v.isSelect = false;return v});
+	  _.map(returnValue.players[0].currentCard, function(v){v.isSelect = false;return v});
+	  _.map(returnValue.players[1].currentCard, function(v){v.isSelect = false;return v});
+      broadcast(returnValue);
+  });
+
+  socket.on('exchange', function(msg){
+	  console.log('player ' + id + ' exchange');
+	  console.log(msg);
+	  returnValue.sceneType = 'turnOver';
+	  returnValue.market = msg.market;
+	  returnValue.players = msg.players;
+	  _.map(returnValue.market, function(v){v.isSelect = false;return v});
+	  _.map(returnValue.players[0].currentCard, function(v){v.isSelect = false;return v});
+	  _.map(returnValue.players[1].currentCard, function(v){v.isSelect = false;return v});
+      broadcast(returnValue);
+  });
+
+  socket.on('getAllCamel', function(msg){
+	  console.log('player ' + id + ' get all camel');
+	  console.log(msg);
+	  returnValue.sceneType = 'turnOver';
+	  returnValue.market = msg.market;
+	  if (returnValue.market.length < 5) {
+	      var length = returnValue.market.length;
+	      for (var i = 0; i < 5 - length; i++) {
+		      returnValue.market.push({type:cards.pop().type, isSelect:false})
+		  }
+	  }
+	  returnValue.players = msg.players;
+	  _.map(returnValue.market, function(v){v.isSelect = false;return v});
+	  _.map(returnValue.players[0].currentCard, function(v){v.isSelect = false;return v});
+	  _.map(returnValue.players[1].currentCard, function(v){v.isSelect = false;return v});
+      broadcast(returnValue);
+  });
+
+  socket.on('turnOver', function(msg){
+	  console.log('player ' + id + ' turnOver');
+	  console.log(msg);
+	  returnValue.sceneType = 'gaming';
+	  returnValue.turn = returnValue.turn % 2 + 1;
+	  returnValue.market = msg.market;
+	  returnValue.players = msg.players;
+	  _.map(returnValue.market, function(v){v.isSelect = false;return v});
+	  _.map(returnValue.players[0].currentCard, function(v){v.isSelect = false;return v});
+	  _.map(returnValue.players[1].currentCard, function(v){v.isSelect = false;return v});
+      broadcast(returnValue);
+  });
+
+  
+  socket.on('roundOver', function(msg){
+	  console.log('roundOver');
+	  console.log(msg);
+	  returnValue.sceneType = 'gaming';
+	  var sum0 = _.reduce(msg.players[0].signs, function(mem, v){
+		  mem += _.reduce(v.values, function(aMem, value){aMem += value;return aMem;}, 0);
+		  return mem;
+	  }, 0);
+	  var sum1 = _.reduce(msg.players[1].signs, function(mem, v){
+		  mem += _.reduce(v.values, function(aMem, value){aMem += value;return aMem;}, 0);
+		  return mem;
+	  }, 0);
+	  var camel0 = _.reduce(msg.players[0].currentCard, function (mem,v){if (v.type === 7){mem++;}return mem;}, 0);
+	  var camel1 = _.reduce(msg.players[1].currentCard, function (mem,v){if (v.type === 7){mem++;}return mem;}, 0);
+	  if (camel0 > camel1) {
+	      sum0 += 5;
+	  } else if (camel1 < camel0) {
+	      sum1 += 5;
+	  }
+	  if (sum0 > sum1) {
+	      msg.players[0].winSign += 1;
+		  if (msg.players[0].winSign === 2) {
+			  returnValue.sceneType = 'gameOver';
+		  }
+	      returnValue.turn = msg.players[1].id;
+	  } else if (sum0 < sum1) {
+		  msg.players[1].winSign += 1;
+		  if (msg.players[1].winSign === 2) {
+			  returnValue.sceneType = 'gameOver';
+		  }
+	      returnValue.turn = msg.players[0].id;
+	  }
+	  returnValue.players = msg.players;
+	  initCards();
+	  initSigns();
+	  returnValue.cards = cards;
+	  returnValue.players[0].currentCard = [];
+	  zeroSign(returnValue.players[0]);
+	  returnValue.players[1].currentCard = [];
+	  zeroSign(returnValue.players[1]);
+	  for (var i = 0; i < 5; i++) {
+	      returnValue.players[0].currentCard.push({type:cards.pop().type, isSelect:false});
+		  returnValue.players[1].currentCard.push({type:cards.pop().type, isSelect:false});
+	  }
+	  returnValue.market = [{type:7, isSelect:false},{type:7, isSelect:false},{type:7, isSelect:false},{type:cards.pop().type, isSelect:false},{type:cards.pop().type, isSelect:false}];
+	  returnValue.signs = signs;
+      broadcast(returnValue);
+  });
+
+  socket.on('disconnect', function(){
+      console.log('player ' + id + ' disconnect');
+      playerPosition[position] = 0;
+      playerNumber--;
+      players = _.filter(players, function(v){
+	      return v.id != id;
+      });
+      _.map(players, function(v) {
+	      v.state = 'logined';
+      });
+      returnValue.sceneType = 'ready';
+      returnValue.players = players;
+	  returnValue.turn = undefined;
+      broadcast(returnValue);
+  });
+
+  socket.on('close', function(){
+      console.log('bbb');
   });
 });
 
--- public/js/game.main.js
+++ public/js/game.main.js
@@ -1,41 +1,476 @@
 $(document).ready(function(){
-    var scene = new THREE.Scene();
+	var renderer, loader, material;
+	var cameraOrtho, sceneOrtho;
 
-    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
-    
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0xEEEEEE);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+	var player1, player2, readyBtn1, readyBtn2;
+	var objects = [];
+	var canvas = document.createElement('canvas');
+	var context = canvas.getContext('2d');
+	var sceneType = 'ready';
+	var mouse;
+	var raycaster;
+	var player, players = [], market, otherPlayer, turn, signs, cardLeft, cards;
+	var oneTexture, twoTexture, threeTexture, fourTexture, fiveTexture, sixTexture, sevenTexture, backTexture;
 
-    var axes = new THREE.AxisHelper(20);
-    scene.add(axes);
+	var ws = io.connect('http://localhost:3000');
 
-    var planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1);
-    var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
-    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+	ws.on('connect', function () {
+	        ws.emit('login');
+	});
+	init();
+	animate();
 
-    plane.rotation.x = -0.5 * Math.PI;
-    plane.position.x = 15;
-    plane.position.y = 0;
-    plane.position.z = 0;
+	function init() {
+		var width = window.innerWidth;
+		var height = window.innerHeight;
+		mouse = new THREE.Vector2();
+		raycaster = new THREE.Raycaster();
 
-    scene.add(plane);
+		cameraOrtho = new THREE.OrthographicCamera( - width / 2, width / 2, height / 2, - height / 2, 1, 10 );
+		cameraOrtho.position.z = 10;
+		sceneOrtho = new THREE.Scene();
+		sceneGamePad = new THREE.Scene();
+		loader = new THREE.TextureLoader();
 
-    var cubeGeometry = new THREE.CubeGeometry(4, 4, 4);
-    var cubeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+		loader.load( "../img/char.png", createHUDSprites );
+		oneTexture = loader.load( "../img/diamond.png");
+		twoTexture = loader.load( "../img/gold.png");
+		threeTexture = loader.load( "../img/silver.png");
+	        fourTexture = loader.load( "../img/leather.png");
+		fiveTexture = loader.load( "../img/spice.png");
+		sixTexture = loader.load( "../img/cloth.png");
+		sevenTexture = loader.load( "../img/camel.png");
+		backTexture = loader.load( "../img/back.png");
 
-    cube.position.x = -4;
-    cube.position.y = 3;
-    cube.position.z = 0;
+		renderer = new THREE.WebGLRenderer();
+		renderer.setClearColor(0xffffff);
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		renderer.autoClear = false;
 
-    scene.add(cube);
+		document.body.appendChild( renderer.domElement );
 
-    camera.position.x = -30;
-    camera.position.y = 40;
-    camera.position.z = 30;
-    camera.lookAt(scene.position);
+		window.addEventListener( 'resize', onWindowResize, false );
+		renderer.domElement.addEventListener( 'mousedown', onDocumentMouseDown, false );
+	}
 
-    $("#WebGL-output").append(renderer.domElement);
-    renderer.render(scene, camera);
+	function createHUDSprites ( texture ) {
+		material = new THREE.SpriteMaterial( { map: texture } );
+		var height = window.innerHeight / 2 - 100;
+		var width = material.map.image.width / material.map.image.height * height;
+		
+		player1 = new THREE.Sprite( material );
+		player1.scale.set( width, height, 1 );
+		sceneOrtho.add(player1);
+		
+		player2 = new THREE.Sprite( material );
+		player2.scale.set( width, height, 1 );
+		sceneOrtho.add(player2);
+
+		$('.ready-one').css('left', window.innerWidth / 2 + width / 2 + 50);
+		$('.ready-one').css('top', window.innerHeight / 2 + window.innerHeight / 4);
+		$('.ready-two').css('left', window.innerWidth / 2 + width / 2 + 50);
+		$('.ready-two').css('top', window.innerHeight / 2 - window.innerHeight / 4);
+
+		updateHUDSprites();
+	}
+
+	function createCard(cardObject, order, height) {
+		var cardMaterial;
+		switch(cardObject.type) {
+			case 1:
+			    cardMaterial = new THREE.SpriteMaterial( { map: oneTexture } );
+			    break;
+			case 2:
+			    cardMaterial = new THREE.SpriteMaterial( { map: twoTexture } );
+			    break;
+			case 3:
+			    cardMaterial = new THREE.SpriteMaterial( { map: threeTexture } );
+			    break;
+			case 4:
+			    cardMaterial = new THREE.SpriteMaterial( { map: fourTexture } );
+			    break;
+			case 5:
+			    cardMaterial = new THREE.SpriteMaterial( { map: fiveTexture } );
+		            break;
+			case 6:
+			    cardMaterial = new THREE.SpriteMaterial( { map: sixTexture } );
+			    break;
+			case 7:
+			    cardMaterial = new THREE.SpriteMaterial( { map: sevenTexture } );
+			    break;
+		}
+		var card = new THREE.Sprite(cardMaterial);
+		card.scale.set(128, 180, 1);
+		if (cardObject.isSelect) {
+		    card.position.set(140 * order - 280, -height + 100, 1);
+		} else {
+		    card.position.set(140 * order - 280, -height, 1);
+		}
+		card.userObject = cardObject;
+		sceneGamePad.add(card);
+		objects.push(card);
+	}
+
+	function createBackCard(order) {
+	    material = new THREE.SpriteMaterial( { map: backTexture } );
+		var card = new THREE.Sprite(material);
+		card.scale.set(128, 180, 1);
+		card.position.set(140 * order - 280, 200, 1);
+		sceneGamePad.add(card);
+	}
+
+	function onDocumentTouchStart( event ) {
+		event.preventDefault();
+		event.clientX = event.touches[0].clientX;
+		event.clientY = event.touches[0].clientY;
+		onDocumentMouseDown( event );
+	}
+
+	function onDocumentMouseDown( event ) {
+		event.preventDefault();
+		mouse.x = ( event.clientX / renderer.domElement.width) * 2 - 1;
+		mouse.y = - ( event.clientY / renderer.domElement.height) * 2 + 1;
+		raycaster.setFromCamera( mouse, cameraOrtho );
+		var intersects = raycaster.intersectObjects(objects);
+		if ( intersects.length > 0 ) {
+			var object = intersects[0].object;
+			if (Math.abs(object.position.x + renderer.domElement.width / 2 - event.clientX) < object.scale.x / 2
+					&& Math.abs(- object.position.y + renderer.domElement.height / 2 - event.clientY) < object.scale.y / 2) {
+			    object.userObject.isSelect = !object.userObject.isSelect;
+			}
+		}
+		render();
+	}
+
+	function updateHUDSprites () {
+		var imageHeight = window.innerHeight / 4;
+
+		var height = window.innerHeight / 2 - 100;
+		var width = material.map.image.width / material.map.image.height * height;
+
+		player1.scale.set(width, height, 1);
+		player2.scale.set(width, height, 1);
+		player1.position.set(0, imageHeight, 1);
+		player2.position.set(0, -imageHeight, 1);
+		$('.ready-one').css('left', window.innerWidth / 2 + width / 2 + 50);
+		$('.ready-one').css('top', window.innerHeight / 2 + window.innerHeight / 4);
+		$('.ready-two').css('left', window.innerWidth / 2 + width / 2 + 50);
+		$('.ready-two').css('top', window.innerHeight / 2 - window.innerHeight / 4);
+		$('.this-one').css('top', window.innerHeight / 2 + window.innerHeight / 4);
+		$('.this-two').css('top', window.innerHeight / 2 - window.innerHeight / 4);
+		$('.this-one').css('left', window.innerWidth / 2 - width / 2 - 100);
+		$('.this-two').css('left', window.innerWidth / 2 - width / 2 - 100);
+	}
+
+	function onWindowResize() {
+		var width = window.innerWidth;
+		var height = window.innerHeight;
+
+		cameraOrtho.left = - width / 2;
+		cameraOrtho.right = width / 2;
+		cameraOrtho.top = height / 2;
+		cameraOrtho.bottom = - height / 2;
+		cameraOrtho.updateProjectionMatrix();
+
+		updateHUDSprites();
+
+		renderer.setSize( window.innerWidth, window.innerHeight );
+	}
+
+	function animate() {
+		render();
+	}
+
+	function checkTurnOver() {
+		if (sceneType === 'turnOver' && turn === player.id) {
+			var count = 0;
+			_.each(signs, function(v) {
+					if (v.values.length === 0) {
+					    count++;
+						console.log(count);
+					}
+			});
+			if (count >= 3) {
+			    ws.emit('roundOver', {'players':players, 'market':market});
+			} else {
+				if (cards.length === 0 && market.length < 5) {
+			        ws.emit('roundOver', {'players':players, 'market':market});
+				} else {
+					if (_.reject(player.currentCard, function(v){return v.type === 7}).length > 6) {
+						window.alert("手牌超过上限，请弃牌");
+						sceneType = 'dropCard';
+						render();
+					} else {
+			            ws.emit('turnOver', {'players':players, 'market':market});
+					}
+				}
+			}
+		}
+	}
+
+	function render() {
+		if (sceneType === 'ready') {
+		    renderer.clear();
+		    renderer.render( sceneOrtho, cameraOrtho );
+	            $('.ready-one').html('等待玩家');
+	            $('.ready-two').html('等待玩家');
+		    _.map(players, function(v){
+		        if (v.id === 1) {
+			    if (v.state === 'logined') {
+			        $('.ready-one').html('请准备');
+			    } else if (v.state === 'ready') {
+			        $('.ready-one').html('已经准备');
+			    }
+			} else if (v.id === 2) {
+			    if (v.state === 'logined') {
+			        $('.ready-two').html('请准备');
+			    } else if (v.state === 'ready') {
+			        $('.ready-two').html('已经准备');
+			    }
+			}
+		    });
+		    $('.ready').css('display', 'block');
+			$('.player-info').css('display', 'none');
+		} else if (sceneType === 'gaming' || sceneType === 'gaining' || sceneType === 'selling' || sceneType == 'turnOver' || sceneType === 'dropCard') {
+		    sceneGamePad.children = [];
+			objects = [];
+			var playerCardGroup = _.partition(player.currentCard, function(v){return v.type === 7});
+			player.camelNumber = playerCardGroup[0].length;
+		    _.each(playerCardGroup[1], function(v,i){
+		        createCard(v,i, 200);
+		    });
+		    _.each(market, function(v,i) {
+			    createCard(v, i, 0);
+		    });
+			var otherCardGroup = _.partition(otherPlayer.currentCard, function(v){return v.type === 7});
+		    _.each(otherCardGroup[1], function(v,i){
+			    createBackCard(i);
+	        });
+		    renderer.clear();
+		    renderer.render( sceneGamePad, cameraOrtho );
+			$('.camel-number span').html(playerCardGroup[0].length);
+			$('.diamond span').html(JSON.stringify(_.findWhere(player.signs, {type:1}).values));
+			$('.gold span').html(JSON.stringify(_.findWhere(player.signs, {type:2}).values));
+			$('.silver span').html(JSON.stringify(_.findWhere(player.signs, {type:3}).values));
+			$('.leathear span').html(JSON.stringify(_.findWhere(player.signs, {type:4}).values));
+			$('.spice span').html(JSON.stringify(_.findWhere(player.signs, {type:5}).values));
+			$('.cloth span').html(JSON.stringify(_.findWhere(player.signs, {type:6}).values));
+			$('.threeSign span').html(JSON.stringify(_.findWhere(player.signs, {type:8}).values));
+			$('.fourSign span').html(JSON.stringify(_.findWhere(player.signs, {type:9}).values));
+			$('.fiveSign span').html(JSON.stringify(_.findWhere(player.signs, {type:10}).values));
+			$('.win span').html(player.winSign);
+            $('.gaming').css('display', 'none');
+            $('.gaining').css('display', 'none');
+			$('.message-info').css('display', 'none');
+			$('.drop-card').css('display', 'none');
+			if (sceneType === 'gaming' && turn === player.id) {
+				$('.gaming').css('display', 'block');
+			} else if (sceneType === 'gaining' && turn === player.id) {
+				$('.gaining').css('display', 'block');
+			} else {
+                $('.message-info').css('display', 'block');
+			}
+			$('.ready').css('display', 'none');
+			$('.player-info').css('display', 'block');
+			$('.card-left').css('display', 'block');
+			$('.card-left span').html(cards.length);
+			if (sceneType === 'dropCard') {
+		        $('.gaining').css('display', 'none');
+		        $('.gaming').css('display', 'none');
+		        $('.message-info').css('display', 'none');
+				$('.drop-card').css('display', 'block');
+			}
+		} else if (sceneType === 'gameOver') {
+			if (player.winSign === 2) {
+				window.alert("你赢了");
+			} else {
+				window.alert("你输了");
+			}
+		}
+	}	
+
+	$('.ready-one').click(function(){
+		if (player.id == 1) {
+		    ws.emit('ready');
+		}
+	});
+
+	$('.ready-two').click(function(){
+		if (player.id == 2) {
+		    ws.emit('ready');
+		}
+	});
+
+	$('.choose-one').click(function(){
+			sceneType = 'gaining';
+			render();
+	});
+
+	$('.cancel').click(function(){
+			sceneType = 'gaming';
+			render();
+	});
+
+	$('.drop-card').click(function(){
+        var leftCard = _.reject(player.currentCard, function(v){return v.isSelect === true})
+		if (_.reject(leftCard, function(v){return v.type === 7}).length > 6) {
+			window.alert("请选择弃掉的牌");
+	    } else {
+			_.map(players, function(v){
+				if (v.id === player.id) {
+				    player.currentCard = leftCard;
+				    return player;
+				}
+			});
+			ws.emit('turnOver', {'players':players, 'market':market});
+		}
+	});
+
+	$('.choose-two').click(function(){
+			var playerPartition = _.partition(player.currentCard, function(v){return v.isSelect});
+			var awardType = 0;
+			if (playerPartition[0].length === 0) {
+			    window.alert("请选择卡牌出售");
+				return;
+			}
+			if (_.filter(playerPartition[0], function(v){return v.type === 1}).length === 1) {
+			    window.alert("钻石不能单张出售");
+				return;
+			}
+			if (_.filter(playerPartition[0], function(v){return v.type === 2}).length === 1) {
+			    window.alert("黄金不能单张出售");
+				return;
+			}
+			if (_.filter(playerPartition[0], function(v){return v.type === 3}).length === 1) {
+			    window.alert("白银不能单张出售");
+				return;
+			}
+			for (var i = 0; i < playerPartition[0].length; i++) {
+			    var sign = _.findWhere(signs, {type:playerPartition[0][i].type});
+				if (sign.values.length > 0) {
+			        _.map(player.signs, function (v){
+			    		if (v.type === playerPartition[0][i].type) {
+			    		    v.values.push(sign.values.shift());
+			    			return v;
+			    		}
+			    	});
+				}
+			}
+			if (playerPartition[0].length >=5) {
+			    awardType = 10;
+			} else if (playerPartition[0].length >= 4) {
+			    awardType = 9;
+			} else if (playerPartition[0].length >= 3) {
+			    awardType = 8;
+			}
+		    var sign = _.findWhere(signs, {type:awardType});
+			if (sign) {
+				_.map(player.signs, function (v){
+					if (v.type === awardType) {
+						v.values.push(sign.values.shift());
+						return v;
+					}
+				});
+			}
+			player.currentCard = playerPartition[1];
+			_.map(players, function(v){
+				if (v.id === player.id) {
+				    return player;
+				}
+			});
+			ws.emit('sell', {'players':players, 'market':market, 'signs':signs});
+	});
+
+	$('.choose-three').click(function(){
+			var marketPartition = _.partition(market, function (v){return v.isSelect});
+			var playerPartition = _.partition(player.currentCard, function(v){return v.isSelect});
+			if (marketPartition[0].length < 2) {
+			    window.alert("至少选择两张牌进行交换");
+				return;
+			}
+			if (marketPartition[0].length === playerPartition[0].length) {
+				_.each(players, function(v){
+					if (v.id === player.id) {
+						v.currentCard = marketPartition[0].concat(playerPartition[1]);
+					}
+				});
+				market = marketPartition[1].concat(playerPartition[0]);
+			} else if (marketPartition[0].length > playerPartition[0].length) {
+			    if (window.confirm("手牌选择不足，是否使用骆驼")) {
+					if (player.camelNumber >= marketPartition[0].length - playerPartition[0].length) {
+					    var camelInEx;
+						_.each(players, function(v){
+							if (v.id === player.id) {
+							    var cardPartition = _.partition(v.currentCard, function(value){return value.type === 7});
+								var camelPartition = _.partition(cardPartition[0], function(value,i){return i < marketPartition[0].length - playerPartition[0].length});
+								v.currentCard = marketPartition[0].concat(_.reject(playerPartition[1], function(value){return value.type === 7})).concat(camelPartition[1]);
+								camelInEx = camelPartition[0];
+							}
+						});
+						market = marketPartition[1].concat(playerPartition[0]).concat(camelInEx);
+					} else {
+					    window.alert("骆驼数不足");
+						return;
+					}
+				}
+			} else {
+			    window.alert("手牌选中的卡牌不能多于市场中的卡牌");
+				return;
+			}
+			ws.emit('exchange', {'players':players, 'market':market});
+	});
+
+	$('.choose-four').click(function(){
+			var index = _.findIndex(market, {isSelect:true});
+			var selected = _.filter(market, function(v){return v.isSelect === true});
+			if (selected.length > 1) {
+			    window.alert("选择了太多卡牌");
+				return;
+			} else if (selected.length === 0) {
+			    window.alert("为选择卡牌");
+				return;
+			}
+			_.each(players, function(v){
+				if (v.id === player.id) {
+				    v.currentCard.push(market[index]);
+				}
+			});
+			market = _.filter(market, function(v){
+				return v.isSelect === false;
+			});
+			ws.emit('getOne', {'players':players, 'market':market});
+	});
+
+	$('.choose-five').click(function(){
+			var camelPartition = _.partition(market, function (v){return v.type === 7});
+			if (camelPartition[0].length === 0) {
+			    window.alert("市场中没有骆驼");
+				return;
+			}
+			_.each(players, function(v){
+				if (v.id === player.id) {
+				    v.currentCard = v.currentCard.concat(camelPartition[0]);
+				}
+			});
+			market = camelPartition[1];
+			ws.emit('getAllCamel', {'players':players, 'market':market});
+	});
+
+	ws.on('return.state', function (data){
+		sceneType = data.sceneType;
+		signs = data.signs;
+		player = _.findWhere(data.players, {id:data.playerId});
+		otherPlayer = _.find(data.players, function(v){return v.id != data.playerId});
+		players = data.players;
+		market = data.market;
+		turn = data.turn;
+		cards = data.cards;
+		if (data.playerId === 1) {
+		    $('.this-one').css('display','block');
+		} else if (data.playerId === 2) {
+		    $('.this-two').css('display','block');
+		}
+		render();
+		checkTurnOver();
+	});
 });
